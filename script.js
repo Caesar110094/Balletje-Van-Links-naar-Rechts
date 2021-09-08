cecccf90803a299985a3c817f6ff33f313d6b7f7
Caesar Coolen <45482701+Caesar110094@users.noreply.github.com>
2021-09-08 10:01:19 +0000

    Class toegevoegd

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+class Bal{
+  ellipse(x, y,50,50);
+  x = x + v;
+  y = y + v2;
+
+  if(x <= 25 || x >= 475) {
+    v = v * -1;
+  }
+
+  if(y <= 25 || y >= 475) {
+    v2 = v2 * -1;
+  }
+}
+
+
 var x, y, v;
 
 function setup() {
-  createCanvas(500, 400);
+  createCanvas(500, 500);
 
   x = 50;
   y = 200;
@@ -16,16 +31,33 @@ function draw() {
   x = x + v;
   y = y + v2;
 
-  square(225,200,50,50)
+  square(225,225,50,50)
 
   if(x <= 25 || x >= 475) {
     v = v * -1;
   }
 
-  if(y <= 25 || y >= 375) {
+  if(y <= 25 || y >= 475) {
     v2 = v2 * -1;
   }
 
-  if()
+}
+
+function draw() {
+  background(225);
+
+  ellipse(x, y,50,50);
+  x = x + v;
+  y = y + v2;
+
+  square(225,225,50,50)
+
+  if(x <= 25 || x >= 475) {
+    v = v * -1;
+  }
+
+  if(y <= 25 || y >= 475) {
+    v2 = v2 * -1;
+  }
 
 }
